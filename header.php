3adbd9256dcb1a19a1364fa234b4fb155ab16b45
Wiwin Ristanto <wiwinristanto@hotmail.com>
2019-07-22 11:23:08 +0700

    uploud fileadmin

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
     <!-- owl_Carousel -->
     <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
     <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
